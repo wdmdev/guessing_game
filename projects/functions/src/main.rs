fd0a65a8d97a21400cb6610cf713985498e3c7d5
William Marstrand <wdmdevelop@gmail.com>
2024-01-27 19:48:53 +0100

    another function

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function(-5, "test", 'u');
+}
+
+fn another_function(number: i32, text: &str, c: char) {
+    println!("This is a number: {number} \n and this is a text: {text} \n and this is a char: {c}");
 }
