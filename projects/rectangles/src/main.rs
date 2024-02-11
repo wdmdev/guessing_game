63f47696002d3800960f91028c46f934b76147c9
William Marstrand <wdmdevelop@gmail.com>
2024-02-11 19:57:19 +0100

    add struct methods and associated functions

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -5,6 +5,21 @@ struct Rectangle {
     height: f64,
 }
 
+// implement block to define struct functions
+impl Rectangle {
+    fn area (&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, rect:&Rectangle) -> bool {
+        rect.width <= self.width && rect.height <= self.height
+    }
+
+    fn square(size:f64) -> Self {
+        Self { width: size, height: size }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle{
         width: 7.5,
@@ -20,6 +35,21 @@ fn main() {
 
     dbg!(&rect2); //prints to standard error console stream
     println!("Has area: {}", area(&rect2));
+
+    let rect3 = Rectangle {
+        width: 5.,
+        ..rect2
+    };
+
+    println!("{:?}\nHas area: {}", rect3, rect3.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3));
+
+    let square1 = Rectangle::square(1.5);
+
+    println!("{:?}\nHas area: {}", square1, square1.area());
+    println!("Can rect1 hold square1? {}", rect1.can_hold(&square1));
 }
 
 fn area(rectangle: &Rectangle) -> f64 {
