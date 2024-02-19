c2f3e105ec88296534c96155201aad88df94af44
William Marstrand <wdmdevelop@gmail.com>
2024-02-19 22:01:10 +0100

    add match and nested enums

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -1,20 +1,68 @@
-#[derive(Debug)]
 enum IpAddr {
     V4(u8, u8, u8, u8),
     V6(String),
 }
 
+enum Device {
+    Laptop(IpAddr),
+    Mobile(IpAddr),
+    SmartWatch,
+    VRHeadset
+}
+
 fn main() {
-    let ipv4 = IpAddr::V4(0,0,0,0);
-    let ipv6 = IpAddr::V6(String::from("2001:db8::8a2e:370:7334"));
-
-    println!("Hello, world!");
-    println!("My IP V4: {:?}", ipv4);
-    println!("My IP V6: {:?}", ipv6);
-
     //Using the Option enum for Some or None valued variables
     let some_number = Some(5); //Rust infers the i32 type because of value
     let some_char = Some('a'); //Ruset infers the char type
     let absent_number: Option<i32> = None; //We have to declare the type because the value is None
 
+    match some_number { //We have to do match on the option to have logic both for values and None
+        None => println!("I have no number!"),
+        Some(i) => println!("My number is: {}", i),
+    }
+
+    match some_char {
+        None => println!("I have no char!"),
+        Some(c) => println!("My char is: {}", c),
+    }
+
+    match absent_number {
+        None => println!{"I have no number!"},
+        _ => println!("My number is not absent anyways!"),
+    }
+
+
+    println!("Let's look at some devices!");
+
+    let d1 = Device::Laptop(IpAddr::V6(String::from("FE80:CD00:0000:0CDE:1257:0000:211E:729C")));
+    let d2 = Device::Mobile(IpAddr::V4(0,0,0,0));
+    let d3 = Device::SmartWatch;
+    let d4 = Device::VRHeadset;
+
+    check_device(d1);
+    check_device(d2);
+    check_device(d3);
+    check_device(d4);
+
+}
+
+fn check_device(device: Device) {
+    match device {
+        Device::Laptop(ip) => {
+            println!("On the workstation!");
+            match ip {
+                IpAddr::V4(i1, i2, i3, i4) => println!("Your ip is oooold: {},{},{},{}", i1, i2, i3, i4),
+                IpAddr::V6(id) => println!("You have a new and long ip: {}", id),
+            }
+        },
+        Device::Mobile(ip) => {
+            println!("You're going mobile!");
+            match ip {
+                IpAddr::V4(i1, i2, i3, i4) => println!("Your ip is oooold: {}.{}.{}.{}", i1, i2, i3, i4),
+                IpAddr::V6(id) => println!("You have a new and long ip: {}", id),
+            }
+        }
+        Device::SmartWatch => println!("Keep that pulse going!"),
+        Device::VRHeadset => println!("Welcome to the matrix!"),
+    }
 }
